47af8c20838cc1f38732cb32eb874f8365ba0414
Shreyas939 <78093143+Shreyas939@users.noreply.github.com>
2025-01-06 21:14:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-  origin: 'https://note-nest-frontend-cyan.vercel.app/',
+  origin: 'https://note-nest-frontend-cyan.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true  // Enable credentials (cookies, authorization headers) cross-origin
