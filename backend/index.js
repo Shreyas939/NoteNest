0cffe9819705757e4d00610e6745dbc51023857f
Shreyas939 <78093143+Shreyas939@users.noreply.github.com>
2025-01-06 21:07:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-  origin: 'https://note-nest-opal.vercel.app/',
+  origin: 'https://note-nest-opal.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true  // Enable credentials (cookies, authorization headers) cross-origin
