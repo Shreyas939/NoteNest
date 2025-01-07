c730ba30b51dead937b65c9422352c89eb8c8dd9
Shreyas939 <shreyasnaik5454@gmail.com>
2025-01-07 11:58:48 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Important for Vercel deployments
+  server: {
+    port: 3000,
+  },
   build: {
-    outDir: "dist", // Default output folder
+    outDir: 'dist',
   },
 });
