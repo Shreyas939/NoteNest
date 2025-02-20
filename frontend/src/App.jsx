f52cc17c443e443600ce7be99f36ffe60daecebb
Shreyas939 <shreyasnaik5454@gmail.com>
2025-02-20 20:19:57 +0530

    b

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home.jsx';
+import React from 'react'
+import Home from "./pages/Home/Home.jsx"
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import Login from './pages/Login/Login.jsx';
 import SignUp from './pages/SignUp/SignUp.jsx';
-import Navbar from './components/Navbar/Navbar';
-import './App.css';
+
+
+const routes = (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/signup" exact element={<SignUp />} />
+        <Route path="/" exact element={<Login />} /> 
+      </Routes>
+    </Router>
+);
+
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/dashboard" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-};
+    <div>
+      {routes}
+    </div>
+  )
+}
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+
+)
