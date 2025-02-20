50413d3a58dbc08efdf9bb43b24bd1998c0dfdd1
Shreyas939 <shreyasnaik5454@gmail.com>
2025-02-20 20:14:54 +0530

    reloaderrorsolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
-import Home from "./pages/Home/Home.jsx"
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login.jsx';
 import SignUp from './pages/SignUp/SignUp.jsx';
-
-
-const routes = (
-    <Router>
-      <Routes>
-        <Route path="/dashboard" exact element={<Home />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/signup" exact element={<SignUp />} />
-        <Route path="/" exact element={<Login />} /> 
-      </Routes>
-    </Router>
-);
-
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      {routes}
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
