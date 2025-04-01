72f5fdc25b8220af7796ae77782c15e1d66e3c29
Shreyas939 <shreyasnaik5454@gmail.com>
2025-04-01 20:58:43 +0530

    pwaprompt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext';
+import PWAPrompt from 'react-ios-pwa-prompt';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
+    <App /><PWAPrompt copyTitle='PWA Ready'/>
   </AuthProvider>,
 
 )
